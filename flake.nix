3ece6c59ddbb5ebe5dafdea06cc6e072898f650d
Hiroshi Nakano <hnakano@tlv.co.jp>
2022-01-20 16:40:50 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "An environment to use graphqlviz";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "graphqlviz-environment";
+      shell = ./shell.nix;
+    };
+}
